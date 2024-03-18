0d512b4af4ae519e03b514214ad4b4042d733857
Bruna Peres <brunaperes@rede.ulbra.br>
2024-03-17 21:23:05 -0300

    primeiro commit

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿//1. Crie uma variável chamada idade e atribua a ela o valor 35 e exiba o seu valor
+byte idade = 35;
+Console.WriteLine(idade);
+
+//2. Crie uma variável chamada nome e atribua o valor "Maria" e exiba o seu valor
+string nome = "Maria";
+Console.WriteLine(nome);
+
+//3.
+float altura = 3.45f;
+Console.WriteLine(altura);
+
+//4.
+const byte ano = 12;
+Console.WriteLine(ano);
+
+//5.
+double? num = 7.80;
+Console.WriteLine(num);
+
+
+/*6. Quais as diferenças entre os tipos por valor e os tipos por referência?
+Resposta: Variáveis de tipos de referência armazenam referências em seus dados e variáveis de tipos de valor contém diretamente seus dados.
+
+7.O que é um nullable type e qual a sua utilidade?
+Resposta: É um recurso do C# que nos permite atribuir o valor null a um tipo de dado que, por padrão, não aceita valores nulos: os tipos primitivos. 
+
+8.O que é Camel Case? Dê um exemplo de sua aplicação
+Resposta:Camel case é um estilo de nomenclatura de texto usado em programação que combina várias palavras em uma única palavra, em que a primeira 
+letra de cada palavra é escrita em minúscula, exceto a primeira palavra. Exemplo: "idadeDaPessoa"
+
+9.O que é Pascal Case? Dê um exemplo de sua aplicação
+Resposta: Pascal case é outro estilo de nomenclatura de texto usado em programação. Ele também combina várias palavras em uma única palavra, mas a 
+primeira letra de cada palavra é escrita em maiúscula. Exemplo: "Carro"
+*/
+
+//10. 
+int x = 77;
+int y = 66;
+Console.WriteLine(x + y);
+
+/*11. Quais os valores padrões dos tipos de dados bool, char, int, double, float, decimal e
+string?
+Resposta: 1. Bool: O valor padrão do tipo bool é false. 
+2. Char: O valor padrão do tipo char é \0 , ou seja, U+0000.
+3. Int: O valor padrão de cada tipo integral é zero, 0 
+4: Double: O valor padrão de Double é 0.
+5. Float O valor padrão de cada tipo de ponto flutuante é zero, 0.
+6. Decimal: O valor padrão de Decimal é 0.
+7. String: O valor padrão de String é Nothing (uma referência nula).
+*/
+
